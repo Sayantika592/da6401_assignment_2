77af341702313dba2b1e5e41909f94c1c8a586c0
Sayantika592 <me22b190@smail.iitm.ac.in>
2026-04-11 21:34:42 +0530

    Delete multitask.py

--- multitask.py
+++
@@ -1,7 +0,0 @@
-"""Top-level re-export for autograder compatibility.
-
-The autograder imports: from multitask import MultiTaskPerceptionModel
-"""
-from models.multitask import MultiTaskPerceptionModel
-
-__all__ = ["MultiTaskPerceptionModel"]
