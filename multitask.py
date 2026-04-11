e5da9c5d6fcd6065c5776037c7bc1b717cdd0d67
Sayantika Chakraborty <me22b190@smail.iitm.ac.in>
2026-04-11 21:26:21 +0530

    Multitask addition

---
+++ multitask.py
@@ -0,0 +1,7 @@
+"""Top-level re-export for autograder compatibility.
+
+The autograder imports: from multitask import MultiTaskPerceptionModel
+"""
+from models.multitask import MultiTaskPerceptionModel
+
+__all__ = ["MultiTaskPerceptionModel"]
